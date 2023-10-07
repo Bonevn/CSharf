aa3ab80460614913ceef4ab3f810d74036c7814c
Bone <tuanlucdzai@gmail.com>
2023-10-07 15:09:30 +0700

    BTVN

---
+++ TextBox/Bai7/Bai7/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string pheptoan = "";
+
+        private void so1_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "1";
+            textBox1.Text = number;
+        }
+
+        private void so2_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "2";
+            textBox1.Text = number;
+        }
+
+        private void so9_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "9";
+            textBox1.Text = number;
+        }
+
+        private void so4_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "4";
+            textBox1.Text = number;
+        }
+
+        private void so5_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "5";
+            textBox1.Text = number;
+        }
+
+        private void so6_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "6";
+            textBox1.Text = number;
+        }
+
+        private void so7_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "7";
+            textBox1.Text = number;
+        }
+
+        private void so8_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "8";
+            textBox1.Text = number;
+        }
+
+        private void so3_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "3";
+            textBox1.Text = number;
+        }
+        private void button18_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "0";
+            textBox1.Text = number;
+        }
+        private void daucong_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "+";
+            textBox1.Text = number;
+            pheptoan = "+";
+        }
+
+        private void dautru_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "-";
+            textBox1.Text = number;
+            pheptoan = "-";
+        }
+
+        private void daunhan_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "*";
+            textBox1.Text = number;
+            pheptoan = "*";
+        }
+
+        private void dauchia_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += "/";
+            textBox1.Text = number;
+            pheptoan = "/";
+        }
+        private void button16_Click(object sender, EventArgs e)
+        {
+            String number = textBox1.Text;
+            number += ".";
+            textBox1.Text = number;
+        }
+        private void daubang_Click(object sender, EventArgs e)
+        {
+            string number = textBox1.Text;
+            string[] socantinhs = number.Split(Convert.ToChar(pheptoan));
+            double ketqua = Convert.ToDouble(socantinhs[0]);
+
+            if (pheptoan == "+")
+            {
+                for (int i = 1; i < socantinhs.Length; i++)
+                {
+                    ketqua += Convert.ToDouble(socantinhs[1]);
+                }
+            }
+            else if (pheptoan == "-")
+            {
+                for (int i = 1; i < socantinhs.Length; i++)
+                {
+                    ketqua -= Convert.ToDouble(socantinhs[1]);
+                }
+            }
+            else if (pheptoan == "*")
+            {
+                for (int i = 1; i < socantinhs.Length; i++)
+                {
+                    ketqua -= Convert.ToDouble(socantinhs[1]);
+                }
+            }
+            else if (pheptoan == "/")
+            {
+                for (int i = 1; i < socantinhs.Length; i++)
+                {
+                    ketqua /= Convert.ToDouble(socantinhs[1]);
+                }
+            }
+
+            textBox1.Text = Convert.ToString(ketqua);
+        }
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
---
+++ TextBox/Bai8/Bai8/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult xacnhan = MessageBox.Show("Bạn có muốn đóng chương trình này không? ", "Xác nhận đóng", (MessageBoxButtons.YesNo), MessageBoxIcon.Question);
+            if (xacnhan == DialogResult.Yes) Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int soluong = int.Parse(txtsoluong.Text);
+            if (soluong > 0)
+            {
+                int dongia = int.Parse(txtdongia.Text);
+                int thanhtien = soluong * dongia;
+
+                txtthanhtien.Text = thanhtien.ToString();
+            }
+            else
+            {
+                txtsoluong.Clear();
+                MessageBox.Show("Vui lòng nhập giá trị lớn hơn 0", "Warming", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtsoluong.Clear();
+            txtdongia.Clear();
+            txtthanhtien.Clear();
+            txttenhang.Clear();
+            txttenhang.Focus();
+        }
+
+        private void txtsoluong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 'A' && e.KeyChar<='Z') || (e.KeyChar >= 'a' && e.KeyChar <= 'z'))
+            {
+                e.Handled = true;
+                MessageBox.Show("Vui lòng nhập số cho giá trị này", "Nhập số", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
