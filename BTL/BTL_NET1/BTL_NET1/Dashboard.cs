bd228812d2a69effcaa049bbbb242961f6400f7f
Bone <tuanlucdzai@gmail.com>
2023-10-28 15:26:03 +0700

    BTL Update

---
+++ BTL/BTL_NET1/BTL_NET1/Dashboard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_NET1
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            Add addForm = new Add();
+            addForm.ShowDialog();
+            dgvDanhSachCanBo.DataSource = null; // Xóa dữ liệu cũ
+
+            dgvDanhSachCanBo.DataSource = ListDanhSachCanBo.Instance.ListDanhsachcb1;
+
+        }
+    }
+}
---
+++ BTL/BTL_NET1/BTL_NET1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_NET1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string username = txtuser.Text;
+            string password = txtpassword.Text;
+
+            if (username == "admin" & password == "1")
+            {
+                txtpassword.Clear();
+                Dashboard ds = new Dashboard();
+                this.Hide();
+                ds.Show();
+            }
+            else
+            {
+                labelerror.Visible = true;
+            }
+
+        }
+
+    }
+}
